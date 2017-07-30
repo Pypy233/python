43ed2c11e345d58e19c8bccb88918c7fe5396ffd
py-task <2529716798@qq.com>
2017-07-30 17:28:01 +0800

    …

---
+++ Fluent Python/card.py
@@ -0,0 +1,35 @@
+'''card.py modify the distribution of card'''
+import collections
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+	ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+	suits = 'spades diamonds clubs hearts'.split(' ')
+
+	def __init__(self):
+		self._cards = [Card(rank, suit) for rank in self.ranks for suit in self.suits]
+
+	def __len__(self):
+		return len(self._cards)	
+
+	def __getitem__(self, position):
+		return self._cards[position]
+
+	'''Add other magic methods'''
+	def __setitem__(self, position, value):
+		self._cards[position] = value
+
+	def __delitem__(self, position):
+		del self._cards[position]
+
+'''demo:'''
+beer_card = Card('J', 'diamonds')
+print(beer_card)
+deck = FrenchDeck()
+print('Length:', len(deck))
+print(deck[0])
+from random import choice
+print(choice(deck))
+del(deck[0])
+for card in deck:
+	print(card)
