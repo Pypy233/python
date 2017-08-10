4e5e0f3153dd6c850d1ffc100e047c14e46e3107
py-task <2529716798@qq.com>
2017-08-10 22:40:20 +0800

    dict

---
+++ hfp/dict_demo.py
@@ -0,0 +1,13 @@
+cleese = {}
+palin = dict();
+print(type(cleese))
+print(type(palin))
+cleese['Name'] = 'John'
+cleese['Job'] = 'engineer';
+cleese['Hobby'] = 'program'
+cleese['sex'] = 'man'
+print(cleese)
+palin['Name'] = 'Miachel'
+palin['Job'] = 'artist'
+palin['family members'] = '5'
+print(palin)
