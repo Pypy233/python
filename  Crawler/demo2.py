7d2eef556a18180f6d9e24a7fa969d139cec65d3
py-task <2529716798@qq.com>
2017-08-22 21:46:17 +0800

    Change it to BeautifulSoup(content, ‘html5lib’)

---  Crawler/demo2.py
+++  Crawler/demo2.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), "html.parser")
+bsObj = BeautifulSoup(html.read(), "html5lib")
 print(bsObj.h1)
 '''It seems that I need the html.parser to get rid of the warning.'''
