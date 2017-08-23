469fdfc2e2ba3fadd30cbae7a01fb1ec5ab24bfb
py-task <2529716798@qq.com>
2017-08-23 14:20:24 +0800

    Add sibing

---  Crawler/children_demo.py
+++  Crawler/children_demo.py
@@ -4,3 +4,6 @@ html = urlopen('http://www.pythonscraping.com/pages/page3.html')
 bsObj = BeautifulSoup(html, 'html5lib')
 for child in bsObj.find('table', {'id':'giftList'}).children:
 	print(child)
+print('*************')
+for sibing in bsObj.find('table', {'id':'giftList'}).tr.children:
+	print(sibing)
