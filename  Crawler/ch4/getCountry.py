d16da251a35d1d744d024b794ae81248de86c2bf
py-task <2529716798@qq.com>
2017-08-27 21:29:28 +0800

    getCountry

---
+++  Crawler/ch4/getCountry.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+import json
+import ssl
+context = ssl._create_unverified_context()
+def getCountry(idAddress):
+	response = urlopen('http://freegeoip.net/json/' + idAddress, context = context).read().decode('utf-8')
+	responseJson = json.loads(response)
+	return responseJson.get('country_code')
+
+
+print(getCountry('50.78.253.58'))
