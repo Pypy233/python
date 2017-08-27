ff361ae8243dac9dc26d4893ff23e1f7b590fdb0
py-task <2529716798@qq.com>
2017-08-27 16:30:25 +0800

    demo

---
+++  Crawler/ch3/getNeededLinks.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import ssl
+
+context = ssl._create_unverified_context()
+html = urlopen('http://en.wikipedia.org/wiki/Kevin_Bacon', context = context)
+bsObj = BeautifulSoup(html, 'html5lib')
+for link in bsObj.find('div', {'id': 'bodyContent'}).findAll('a', href = re.compile("^(/wiki/)((?!:).)*$")):
+	if 'href' in link.attrs:
+		print(link['href'])
