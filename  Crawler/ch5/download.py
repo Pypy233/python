d57bd514767f0b15764ced4948eaf9667aab514c
py-task <2529716798@qq.com>
2017-08-28 12:36:51 +0800

    Download the logo.

---
+++  Crawler/ch5/download.py
@@ -0,0 +1,7 @@
+from urllib.request import urlopen
+from urllib.request import urlretrieve
+from bs4 import BeautifulSoup
+html = urlopen('http://www.pythonscraping.com')
+bsObj = BeautifulSoup(html, 'html5lib')
+imageLocation = bsObj.find('a', {'id': 'logo'}).find('img')['src']
+urlretrieve(imageLocation, 'logo.jpg')
