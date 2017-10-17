3ffdecfe53dc6014881bafb3c79f87cef090dc33
py-task <2529716798@qq.com>
2017-10-17 14:06:47 +0800

    ？

---  Crawler/ch2/path_demo.py
+++  Crawler/ch2/path_demo.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
-html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+html = urlopen("http://qinggongxiao.nju.edu.cn/cys/index.html")
 bsObj = BeautifulSoup(html, 'html5lib')
 images = bsObj.findAll('img', {'src': re.compile("\.\./img/gifts/img.*\.jpg")})
 for i in images:
