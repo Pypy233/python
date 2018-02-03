49e0f165a82dd064ac0ff014a9bdea674554f7d1
py-task <2529716798@qq.com>
2018-02-03 20:01:36 +0800

    Bug to be fixed, add image login

---  Crawler/actual combat/qq_zone.py
+++  Crawler/actual combat/qq_zone.py
@@ -10,17 +10,24 @@ class QQZoneTool():
         self.user = user
         self.password = password
 
+    # 头像登录或者用户名密码登录
     def login(self):
         driver = webdriver.Chrome()
         driver.get('http://user.qzone.qq.com/' + self.user + '/311')
         time.sleep(5)
+        driver.switch_to.frame('login_frame')
+        try:
+            driver.find_element_by_css_selector('#img_out_' + self.user).click()
+            print('Image login...')
+        except:
+            print('Fail to login by image...')
+
         try:
             driver.find_element_by_id('login_div')
             find_login = True
         except:
             find_login = False
         if find_login:
-            driver.switch_to.frame('login_frame')
             driver.find_element_by_id('switcher_plogin').click()
             driver.find_element_by_id('u').clear()  # 选择用户名框
             driver.find_element_by_id('u').send_keys(self.user)
@@ -31,30 +38,40 @@ class QQZoneTool():
 
         return driver
 
-    # 参考洲神的代码，emmm没有考虑翻页，过几天完善
-    def get_friend_shuoshuo(self, friend_qq):
+    def access_friend_zone(self, friend_qq):
         driver = self.login()
         try:
-            accept = driver.get('http://user.qzone.qq.com/' + friend_qq +'/311'.format(friend_qq))
+            accept = driver.get('http://user.qzone.qq.com/' + friend_qq + '/311'.format(friend_qq))
             accept = True
         except:
             accept = False
-            print('Fail to accept shuoshuo')
+            print('Fail to accept access to qq zone...')
+        driver.switch_to.frame('app_canvas_frame')
+        time.sleep(3)  # 必要的等待
+        return driver
 
-        if accept:
-            driver.switch_to.frame('app_canvas_frame')
-            time.sleep(3)   #必要的等待
-            text_content = driver.find_elements_by_css_selector('.content')
-            shuoshuo_time = driver.find_elements_by_css_selector('.c_tx.c_tx3.goDetail')
-            for con, sti in zip(text_content, shuoshuo_time):
-                data = {
-                    'time': sti.text,
-                    'content': con.text
-                }
-                print(data)
+    # 参考洲神的代码，emmm没有考虑翻页，过几天完善
+    def get_friend_shuoshuo(self, friend_qq):
+        driver = self.access_friend_zone(friend_qq)
+        text_content = driver.find_elements_by_css_selector('.content')
+        shuoshuo_time = driver.find_elements_by_css_selector('.c_tx.c_tx3.goDetail')
+        for con, sti in zip(text_content, shuoshuo_time):
+            data = {
+                'time': sti.text,
+                'content': con.text
+            }
+            print(data)
+
         driver.close()
         driver.quit()
 
     # 点赞狂魔，emmm可能会被打
-    def praise_friend(self, friend_qq):
+    def like_friend(self, friend_qq):
+        driver = self.access_friend_zone(friend_qq)
+        time.sleep(2)
+        driver.find_elements_by_xpath('./div[3]/div[1]/p/a[3]')
+
+        driver.close()
+        driver.quit()
+
 
